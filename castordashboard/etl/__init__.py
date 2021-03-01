ca92660aa71258c44108c9f038fe4674e4a1b449
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-03-01 10:21:16 +0100

    Saving version 0.4.0 before deploying to PyPI. Fixed import issue

--- castordashboard/etl/__init__.py
+++ castordashboard/etl/__init__.py
@@ -1,4 +1,4 @@
 from .script_runner import ScriptRunner
 from .scripts import DummyScript
 from .scripts import RetrieveStudyListScript
-from .scripts import RetrieveDashboardDataScript
+from .scripts import RetrieveHistogramWithProcedureCountsScript
