2b1a6f26b55a156110afec635cbf296c08f32053
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-03-17 09:29:23 +0100

    Use full paths to Castor client ID and secret files

--- castordashboard/scripts/run_etl.sh
+++ castordashboard/scripts/run_etl.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-export CASTOR_CLIENT_ID=$(cat $HOME/castorclientid.txt)
-export CASTOR_CLIENT_SECRET=$(cat $HOME/castorclientsecret.txt)
+# Note: this script is meant to run on scalpel!
+# I'm using hard-coded paths here because that is what the cron job likely requires.
+
+export CASTOR_CLIENT_ID=$(cat /home/local/UNIMAAS/r.brecheisen/castorclientid.txt)
+export CASTOR_CLIENT_SECRET=$(cat /home/local/UNIMAAS/r.brecheisen/castorclientsecret.txt)
 
 docker-compose run etl bash -c "python script_runner.py --param=/data/params.json"
